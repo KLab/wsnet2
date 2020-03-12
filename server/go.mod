9bdafd0abd32ec0f77deccd3ebd1608ea19fb718
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2020-03-12 09:47:20 +0900

    grpc create room definition

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module wsnet2
 
 go 1.13
+
+require (
+	github.com/favadi/protoc-go-inject-tag v1.0.0
+	github.com/golang/protobuf v1.3.4
+)
---
+++ server/tools.go
@@ -0,0 +1,7 @@
+// +build tools
+package wsnet2
+
+import (
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/favadi/protoc-go-inject-tag"
+)
