82b983d731bd2204efc502de1571b46977553a1f
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2020-03-26 12:10:38 +0900

    make test

--- server/tools.go
+++ server/tools.go
@@ -1,7 +1,8 @@
 // +build tools
+
 package wsnet2
 
 import (
-	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/favadi/protoc-go-inject-tag"
+	_ "github.com/golang/protobuf/protoc-gen-go"
 )
