759309f1d064986aa5f212d2b70d9106bae6466e
YAMAMOTO Masaya <yamamoto-ma@klab.com>
2020-05-21 01:29:52 +0900

    gRPCを投げてProtoBufのレスポンスを返す（仮）

--- server/lobby/room.go
+++ server/lobby/room.go
@@ -1,8 +1,13 @@
 package lobby
 
 import (
-	"github.com/jmoiron/sqlx"
+	"context"
+	"fmt"
 
+	"github.com/jmoiron/sqlx"
+	"google.golang.org/grpc"
+
+	"wsnet2/log"
 	"wsnet2/pb"
 )
 
@@ -19,6 +24,12 @@ type RoomService struct {
 	wsPort   int
 }
 
+func NewRoom(info *pb.RoomInfo) *Room {
+	return &Room{
+		RoomInfo: info,
+	}
+}
+
 func NewRoomService(db *sqlx.DB, grpcPort, wsPort int) *RoomService {
 	rs := &RoomService{
 		db:       db,
@@ -26,4 +37,65 @@ func NewRoomService(db *sqlx.DB, grpcPort, wsPort int) *RoomService {
 		wsPort:   wsPort,
 	}
 	return rs
+}
+
+func makeRoomURL(host, roomID string, port int) string {
+	return fmt.Sprintf("ws://%s:%d/room/%s", host, port, roomID)
+}
+
+func makeRoomHost(host string, port int) string {
+	return fmt.Sprintf("%s:%d", host, port)
+}
+
+type gameServer struct {
+	id         int
+	hostName   string
+	publicHost string
+}
+
+func (rs *RoomService) Create(appID string, roomOption *pb.RoomOption, clientInfo *pb.ClientInfo) (*pb.Room, error) {
+	// TODO: check App
+
+	// TODO: check Max Rooms
+
+	// TODO: select game server
+
+	gs := &gameServer{
+		id:         1,
+		hostName:   "localhost",
+		publicHost: "localhost",
+	}
+	grpcAddr := fmt.Sprintf("%s:%d", gs.hostName, rs.grpcPort)
+
+	conn, err := grpc.Dial(grpcAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Errorf("client connection error:", err)
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := pb.NewGameClient(conn)
+
+	req := &pb.CreateRoomReq{
+		AppId:      appID,
+		RoomOption: roomOption,
+		MasterInfo: clientInfo,
+	}
+
+	res, err := client.Create(context.TODO(), req)
+	if err != nil {
+		fmt.Printf("create room error: %v", err)
+	}
+
+	// TODO: check response
+
+	log.Infof("Created room: %v", res.RoomInfo)
+
+	room := &pb.Room{
+		RoomInfo: res.RoomInfo,
+	}
+	room.Hostname = makeRoomHost(gs.publicHost, rs.wsPort)
+	room.Url = makeRoomURL(gs.publicHost, room.RoomInfo.Id, rs.wsPort)
+
+	return room, nil
 }
--- server/lobby/room_controller.go
+++ server/lobby/room_controller.go
@@ -2,12 +2,17 @@ package lobby
 
 import (
 	crand "crypto/rand"
+	"io/ioutil"
 	"math"
 	"math/big"
 	"math/rand"
 	"net/http"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/mux"
+
+	"wsnet2/log"
+	"wsnet2/pb"
 )
 
 func init() {
@@ -20,6 +25,39 @@ func (rs *RoomService) RegisterRoutes(r *mux.Router) {
 }
 
 func (rs *RoomService) HandleCreateRoom(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Create Room\n"))
+	appID := r.Header.Get("X-App-Id")
+	userID := r.Header.Get("X-User-Id")
+
+	_, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Errorf("Failed to read request body: %w", err)
+		http.Error(w, "Failed to request body", http.StatusInternalServerError)
+		return
+	}
+	r.Body.Close()
+
+	// TODO: Unmarshal body and set RoomOption
+
+	roomOption := &pb.RoomOption{
+		Visible:   true,
+		Watchable: true,
+		LogLevel:  4,
+	}
+	clientInfo := &pb.ClientInfo{
+		Id: userID,
+	}
+	room, err := rs.Create(appID, roomOption, clientInfo)
+	if err != nil {
+		log.Errorf("Failed to create room: %w", err)
+		http.Error(w, "Failed to create room", http.StatusInternalServerError)
+		return
+	}
+	log.Debugf("%v", room)
+	res, err := proto.Marshal(room)
+	if err != nil {
+		log.Errorf("Failed to marshal room: %w", err)
+		http.Error(w, "Failed to marshal room", http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/protobuf; charset=utf-8")
+	w.Write(res)
 }
