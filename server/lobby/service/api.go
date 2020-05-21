5292019e0296bf1c0faca949de3e95bc379b0824
YAMAMOTO Masaya <yamamoto-ma@klab.com>
2020-05-21 18:01:28 +0900

    HTTP関連の処理をlobby/service配下へ移動

--- server/lobby/service/api.go
+++ server/lobby/service/api.go
@@ -2,14 +2,17 @@ package service
 
 import (
 	"context"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/vmihailenco/msgpack/v4"
 	"golang.org/x/xerrors"
 
 	"wsnet2/log"
+	"wsnet2/pb"
 )
 
 func (sv *LobbyService) serveAPI(ctx context.Context) <-chan error {
@@ -61,5 +64,46 @@ func (sv *LobbyService) registerRoutes(r *mux.Router) {
 		r = r.Host(sv.conf.Hostname).Subrouter()
 	}
 
-	sv.roomService.RegisterRoutes(r)
+	r.HandleFunc("/rooms", sv.handleCreateRoom).Methods("POST")
+}
+
+func (sv *LobbyService) handleCreateRoom(w http.ResponseWriter, r *http.Request) {
+	appID := r.Header.Get("X-App-Id")
+	userID := r.Header.Get("X-User-Id")
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Errorf("Failed to read request body: %w", err)
+		http.Error(w, "Failed to request body", http.StatusInternalServerError)
+		return
+	}
+	r.Body.Close()
+
+	params := make(map[string]interface{})
+	msgpack.Unmarshal(body, &params)
+
+	log.Debugf("%v", params)
+
+	roomOption := &pb.RoomOption{
+		Visible:   true,
+		Watchable: true,
+		LogLevel:  4,
+	}
+	clientInfo := &pb.ClientInfo{
+		Id: userID,
+	}
+	room, err := sv.roomService.Create(appID, roomOption, clientInfo)
+	if err != nil {
+		log.Errorf("Failed to create room: %w", err)
+		http.Error(w, "Failed to create room", http.StatusInternalServerError)
+		return
+	}
+	log.Debugf("%#v", room)
+	res, err := msgpack.Marshal(room)
+	if err != nil {
+		log.Errorf("Failed to marshal room: %w", err)
+		http.Error(w, "Failed to marshal room", http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/x-msgpack; charset=utf-8")
+	w.Write(res)
 }
