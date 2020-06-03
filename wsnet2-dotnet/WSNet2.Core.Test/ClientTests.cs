a81783dc510b9de80783590e1f39cc7af83e097d
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2020-06-03 21:56:37 +0900

    dotnet test project

---
+++ wsnet2-dotnet/WSNet2.Core.Test/ClientTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System.Runtime.Serialization;
+
+namespace WSNet2.Core.Test
+{
+    public class ClientTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
