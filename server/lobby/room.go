9edbcf72c58afb5cc6ab90a4086225ada213cbc1
YAMAMOTO Masaya <yamamoto-ma@klab.com>
2020-06-05 13:48:25 +0900

    roomIdでの検索ではcachedqueryを使わない

--- server/lobby/room.go
+++ server/lobby/room.go
@@ -3,8 +3,6 @@ package lobby
 import (
 	"context"
 	"fmt"
-	"sync"
-	"time"
 
 	"github.com/jmoiron/sqlx"
 	"golang.org/x/xerrors"
@@ -15,20 +13,12 @@ import (
 	"wsnet2/pb"
 )
 
-const (
-	// TODO: どこまで絞る？
-	roomSelectQuery = "SELECT * FROM room WHERE app_id = ? ORDER BY created DESC LIMIT 10000"
-)
-
 type AppID string
 
 type RoomService struct {
 	db   *sqlx.DB
 	conf *config.LobbyConf
 	apps []pb.App
-
-	muRoomQueries sync.Mutex
-	roomQueries   map[AppID]*CachedQuery // by appid
 
 	gameQuery *GameQuery
 }
@@ -41,56 +31,12 @@ func NewRoomService(db *sqlx.DB, conf *config.LobbyConf) (*RoomService, error) {
 		return nil, xerrors.Errorf("select apps error: %w", err)
 	}
 	rs := &RoomService{
-		db:          db,
-		conf:        conf,
-		apps:        apps,
-		roomQueries: make(map[AppID]*CachedQuery),
-		gameQuery:   NewGameQuery(db, conf.ValidHeartBeat),
+		db:        db,
+		conf:      conf,
+		apps:      apps,
+		gameQuery: NewGameQuery(db, conf.ValidHeartBeat),
 	}
 	return rs, nil
-}
-
-func scanRooms(rows *sqlx.Rows) (interface{}, error) {
-	defer rows.Close()
-
-	rooms := map[string]*pb.RoomInfo{}
-	for rows.Next() {
-		var room pb.RoomInfo
-		err := rows.StructScan(&room)
-		if err != nil {
-			return rooms, err
-		}
-		rooms[room.Id] = &room
-	}
-	return rooms, nil
-}
-
-func (rs *RoomService) getRooms(appId string) (map[string]*pb.RoomInfo, error) {
-	rs.muRoomQueries.Lock()
-	defer rs.muRoomQueries.Unlock()
-	q := rs.roomQueries[AppID(appId)]
-	if q == nil {
-		// TODO: どのくらいキャッシュしておく？
-		q = NewCachedQuery(rs.db, time.Millisecond*10, scanRooms, roomSelectQuery, appId)
-		rs.roomQueries[AppID(appId)] = q
-	}
-	rooms, err := q.Query()
-	if err != nil {
-		return nil, xerrors.Errorf("cachedquery error: %w", err)
-	}
-	return rooms.(map[string]*pb.RoomInfo), nil
-}
-
-func (rs *RoomService) getRoom(appId, roomId string) (*pb.RoomInfo, error) {
-	rooms, err := rs.getRooms(appId)
-	if err != nil {
-		return nil, err
-	}
-	room, ok := rooms[roomId]
-	if !ok {
-		return nil, xerrors.Errorf("room not found")
-	}
-	return room, nil
 }
 
 func (rs *RoomService) Create(appId string, roomOption *pb.RoomOption, clientInfo *pb.ClientInfo) (*pb.JoinedRoomRes, error) {
@@ -149,7 +95,8 @@ func (rs *RoomService) Join(appId, roomId string, clientInfo *pb.ClientInfo) (*p
 		return nil, xerrors.Errorf("Unknown appId: %v", appId)
 	}
 
-	room, err := rs.getRoom(appId, roomId)
+	var room pb.RoomInfo
+	err := rs.db.Get(&room, "SELECT * FROM room WHERE app_id = ? AND id = ?", appId, roomId)
 	if err != nil {
 		return nil, xerrors.Errorf("Join: failed to get room: %w", err)
 	}
