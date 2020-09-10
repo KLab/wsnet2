abb0f05b16e522165ece699c82ed2fda57b49ab1
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2020-09-10 21:16:00 +0900

    Queryの型違いメソッド

--- wsnet2-unity/Assets/WSNet2/Scripts/Core/Query.cs
+++ wsnet2-unity/Assets/WSNet2/Scripts/Core/Query.cs
@@ -126,6 +126,197 @@ namespace WSNet2.Core
             return this;
         }
 
+        /// <summary>
+        ///   nullかどうかの条件を追加
+        /// </summary>
+        public Query IsNull(string key)
+        {
+            and(new Condition(key, Op.Equal, serialize()));
+            return this;
+        }
+        public Query IsNotNull(string key)
+        {
+            and(new Condition(key, Op.Not, serialize()));
+            return this;
+        }
+
+        /// <summary>
+        ///   boolの条件を追加
+        /// </summary>
+        public Query Equal(string key, bool val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, bool val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+
+        /// <summary>
+        ///   sbyteの条件を追加
+        /// </summary>
+        public Query Equal(string key, sbyte val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, sbyte val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, sbyte val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, sbyte val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, sbyte val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, sbyte val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, sbyte min, sbyte max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   byteの条件を追加
+        /// </summary>
+        public Query Equal(string key, byte val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, byte val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, byte val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, byte val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, byte val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, byte val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, byte min, byte max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   shortの条件を追加
+        /// </summary>
+        public Query Equal(string key, short val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, short val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, short val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, short val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, short val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, short val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, short min, short max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   ushortの条件を追加
+        /// </summary>
+        public Query Equal(string key, ushort val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, ushort val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, ushort val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, ushort val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, ushort val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, ushort val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, ushort min, ushort max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   Intの条件を追加
+        /// </summary>
         public Query Equal(string key, int val)
         {
             and(new Condition(key, Op.Equal, serialize(val)));
@@ -163,7 +354,209 @@ namespace WSNet2.Core
             return this;
         }
 
+        /// <summary>
+        ///   uintの条件を追加
+        /// </summary>
+        public Query Equal(string key, uint val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, uint val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, uint val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, uint val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, uint val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, uint val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, uint min, uint max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
 
+        /// <summary>
+        ///   longの条件を追加
+        /// </summary>
+        public Query Equal(string key, long val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, long val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, long val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, long val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, long val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, long val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, long min, long max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   ulongの条件を追加
+        /// </summary>
+        public Query Equal(string key, ulong val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, ulong val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, ulong val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, ulong val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, ulong val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, ulong val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, ulong min, ulong max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   floatの条件を追加
+        /// </summary>
+        public Query Equal(string key, float val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, float val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, float val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, float val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, float val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, float val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, float min, float max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   doubleの条件を追加
+        /// </summary>
+        public Query Equal(string key, double val)
+        {
+            and(new Condition(key, Op.Equal, serialize(val)));
+            return this;
+        }
+        public Query Not(string key, double val)
+        {
+            and(new Condition(key, Op.Not, serialize(val)));
+            return this;
+        }
+        public Query LessThan(string key, double val)
+        {
+            and(new Condition(key, Op.LessThan, serialize(val)));
+            return this;
+        }
+        public Query LessEqual(string key, double val)
+        {
+            and(new Condition(key, Op.LessEqual, serialize(val)));
+            return this;
+        }
+        public Query GreaterThan(string key, double val)
+        {
+            and(new Condition(key, Op.GreaterThan, serialize(val)));
+            return this;
+        }
+        public Query GreaterEqual(string key, double val)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(val)));
+            return this;
+        }
+        public Query Between(string key, double min, double max)
+        {
+            and(new Condition(key, Op.GreaterEqual, serialize(min)));
+            and(new Condition(key, Op.LessEqual, serialize(max)));
+            return this;
+        }
+
+        /// <summary>
+        ///   stringの条件を追加
+        /// </summary>
         public Query Equal(string key, string val)
         {
             and(new Condition(key, Op.Equal, serialize(val)));
@@ -175,6 +568,7 @@ namespace WSNet2.Core
             return this;
         }
 
+
         private void and(Condition cond)
         {
             foreach (var cs in condsList)
@@ -183,7 +577,128 @@ namespace WSNet2.Core
             }
         }
 
+        private byte[] serialize()
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write();
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(bool val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(sbyte val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(byte val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(short val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(ushort val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
         private byte[] serialize(int val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(uint val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(long val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(ulong val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(float val)
+        {
+            var writer = Serialization.GetWriter();
+            lock(writer)
+            {
+                writer.Reset();
+                writer.Write(val);
+                return writer.ToArray();
+            }
+        }
+
+        private byte[] serialize(double val)
         {
             var writer = Serialization.GetWriter();
             lock(writer)
