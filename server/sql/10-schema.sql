2154506f8e908d8c4d11b3b163232825cdc9808c
Inada Naoki <songofacandy@gmail.com>
2020-09-29 12:43:15 +0900

    hub: schema

--- server/sql/10-schema.sql
+++ server/sql/10-schema.sql
@@ -11,6 +11,18 @@ CREATE TABLE `game_server` (
   UNIQUE KEY `idx_hostname` (`hostname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+DROP TABLE IF EXISTS `hub_server`;
+CREATE TABLE `hub_server` (
+  `id`          INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `hostname`    VARCHAR(191) NOT NULL,
+  `public_name` VARCHAR(191) NOT NULL,
+  `grpc_port`   INTEGER NOT NULL,
+  `ws_port`     INTEGER NOT NULL,
+  `status`      TINYINT NOT NULL,
+  `heartbeat`   BIGINT,
+  UNIQUE KEY `idx_hostname` (`hostname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 DROP TABLE IF EXISTS `app`;
 CREATE TABLE app (
   `id`   VARCHAR(32) COLLATE ascii_bin PRIMARY KEY,
@@ -34,4 +46,12 @@ CREATE TABLE room (
   `props` BLOB,
   `created` DATETIME,
   KEY `idx_search_group` (`app_id`, `search_group`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `hub`;
+CREATE TABLE hub (
+  `id`     VARCHAR(32) PRIMARY KEY,
+  `host_id` INTEGER UNSIGNED NOT NULL,
+  `watchers` INTEGER UNSIGNED NOT NULL,
+  `created` DATETIME,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
