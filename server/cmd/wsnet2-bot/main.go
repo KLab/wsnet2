f355c6d63068664392958a7cb48f42bde98e4f9d
YAMAMOTO Masaya <yamamoto-ma@klab.com>
2021-04-13 16:09:44 +0900

    botのコードを別のソースファイルに切り出した

--- server/cmd/wsnet2-bot/main.go
+++ server/cmd/wsnet2-bot/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
-	"net/http"
-	"strconv"
 	"time"
-
-	"github.com/gorilla/websocket"
-	"github.com/vmihailenco/msgpack/v4"
-
-	"wsnet2/auth"
 	"wsnet2/binary"
 	"wsnet2/lobby"
-	"wsnet2/lobby/service"
-	"wsnet2/pb"
+
+	"github.com/gorilla/websocket"
 )
 
 var (
@@ -28,254 +20,15 @@ var (
 	WSNet2Commit  string = "LOCAL"
 )
 
-type bot struct {
-	appId  string
-	appKey string
-	userId string
-	props  binary.Dict
-	ws     *websocket.Dialer
-}
-
-func NewBot(appId, appKey, userId string) *bot {
-	props := binary.Dict{}
-	props["p1"] = binary.MarshalInt(65535)
-	props["p2"] = binary.MarshalStr16("fuga")
-
-	return &bot{
-		appId:  appId,
-		appKey: appKey,
-		userId: userId,
-		props:  props,
-		ws: &websocket.Dialer{
-			Subprotocols:    []string{},
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-		},
-	}
-}
-
-func (b *bot) CreateRoom() (*pb.JoinedRoomRes, error) {
-	props := binary.Dict{}
-	props["key1"] = binary.MarshalInt(1024)
-	props["key2"] = binary.MarshalStr16("hoge")
-
-	param := &service.CreateParam{
-		RoomOption: pb.RoomOption{
-			Visible:     true,
-			Joinable:    true,
-			Watchable:   true,
-			WithNumber:  true,
-			MaxPlayers:  6,
-			SearchGroup: 1,
-			PublicProps: binary.MarshalDict(props),
-		},
-		ClientInfo: pb.ClientInfo{
-			Id:    b.userId,
-			Props: binary.MarshalDict(b.props),
-		},
-	}
-
-	var res service.LobbyResponse
-
-	err := b.doLobbyRequest("POST", "http://localhost:8080/rooms", param, &res)
-	if err != nil {
-		return nil, err
-	}
-	if res.Room == nil {
-		return nil, fmt.Errorf("Create failed: %v", res.Msg)
-	}
-
-	room := res.Room
-	log.Printf("[bot:%v] Create success, WebSocket=%s\n", b.userId, room.Url)
-
-	return room, nil
-}
-
-func (b *bot) JoinRoom(roomId string, queries []lobby.PropQuery) (*pb.JoinedRoomRes, error) {
-	return b.joinRoom(false, roomId, queries)
-}
-
-func (b *bot) WatchRoom(roomId string, queries []lobby.PropQuery) (*pb.JoinedRoomRes, error) {
-	return b.joinRoom(true, roomId, queries)
-}
-
-func (b *bot) joinRoom(watch bool, roomId string, queries []lobby.PropQuery) (*pb.JoinedRoomRes, error) {
-	param := &service.JoinParam{
-		Queries: []lobby.PropQueries{queries},
-		ClientInfo: pb.ClientInfo{
-			Id:    b.userId,
-			Props: binary.MarshalDict(b.props),
-		},
-	}
-
-	var res service.LobbyResponse
-
-	var url string
-	if watch {
-		url = fmt.Sprintf("http://localhost:8080/rooms/watch/id/%s", roomId)
-	} else {
-		url = fmt.Sprintf("http://localhost:8080/rooms/join/id/%s", roomId)
-	}
-	err := b.doLobbyRequest("POST", url, param, &res)
-	if err != nil {
-		return nil, err
-	}
-	if res.Room == nil {
-		return nil, fmt.Errorf("Join failed: %v", res.Msg)
-	}
-
-	room := res.Room
-	log.Printf("[bot:%v] Join success, WebSocket=%s\n", b.userId, room.Url)
-
-	return room, nil
-}
-
-func (b *bot) JoinRoomByNumber(roomNumber int32, queries []lobby.PropQuery) (*pb.JoinedRoomRes, error) {
-	param := &service.JoinParam{
-		Queries: []lobby.PropQueries{queries},
-		ClientInfo: pb.ClientInfo{
-			Id:    b.userId,
-			Props: binary.MarshalDict(b.props),
-		},
-	}
-
-	var res service.LobbyResponse
-
-	url := fmt.Sprintf("http://localhost:8080/rooms/join/number/%d", roomNumber)
-	err := b.doLobbyRequest("POST", url, param, &res)
-	if err != nil {
-		return nil, err
-	}
-	if res.Room == nil {
-		return nil, fmt.Errorf("Join by room number failed: %v", res.Msg)
-	}
-
-	room := res.Room
-	log.Printf("[bot:%v] Join by room number success, WebSocket=%s\n", b.userId, room.Url)
-
-	return room, nil
-}
-
-func (b *bot) JoinRoomAtRandom(searchGroup uint32, queries []lobby.PropQuery) (*pb.JoinedRoomRes, error) {
-	param := &service.JoinParam{
-		Queries: []lobby.PropQueries{queries},
-		ClientInfo: pb.ClientInfo{
-			Id:    b.userId,
-			Props: binary.MarshalDict(b.props),
-		},
-	}
-
-	var res service.LobbyResponse
-
-	url := fmt.Sprintf("http://localhost:8080/rooms/join/random/%d", searchGroup)
-	err := b.doLobbyRequest("POST", url, param, &res)
-	if err != nil {
-		return nil, err
-	}
-	if res.Room == nil {
-		return nil, fmt.Errorf("Join at random failed: %v", res.Msg)
-	}
-
-	room := res.Room
-	log.Printf("[bot:%v] Join at random success, WebSocket=%s\n", b.userId, room.Url)
-	return room, nil
-}
-
-func (b *bot) SearchRoom(searchGroup uint32, queries []lobby.PropQuery) ([]*pb.RoomInfo, error) {
-	param := &service.SearchParam{
-		SearchGroup: searchGroup,
-		Queries:     []lobby.PropQueries{queries},
-	}
-
-	var res service.LobbyResponse
-
-	err := b.doLobbyRequest("POST", "http://localhost:8080/rooms/search", param, &res)
-	if err != nil {
-		log.Printf("error: %v\n", err)
-		return nil, err
-	}
-	if res.Rooms == nil {
-		log.Printf("error: %v\n", res.Msg)
-		return nil, fmt.Errorf("Search failed: %v", res.Msg)
-	}
-
-	rooms := res.Rooms
-	log.Printf("[bot:%v] Search success, rooms=%v\n", b.userId, rooms)
-	return rooms, nil
-}
-
-func (b *bot) doLobbyRequest(method, url string, param, dst interface{}) error {
-	var p bytes.Buffer
-	err := msgpack.NewEncoder(&p).UseJSONTag(true).Encode(param)
-	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest(method, url, &p)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Add("Content-Type", "application/x-msgpack")
-	req.Header.Add("Host", "localhost")
-	req.Header.Add("Wsnet2-App", b.appId)
-	req.Header.Add("Wsnet2-User", b.userId)
-
-	authdata, err := auth.GenerateAuthData(b.appKey, b.userId, time.Now())
-	if err != nil {
-		return err
-	}
-	req.Header.Add("Authorization", "Bearer "+authdata)
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to lobby request: lobby server returned status %v", res.StatusCode)
-	}
-
-	err = msgpack.NewDecoder(res.Body).UseJSONTag(true).Decode(dst)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (b *bot) DialGame(url, authKey string, seq int) (*websocket.Conn, error) {
-	hdr := http.Header{}
-	hdr.Add("Wsnet2-App", b.appId)
-	hdr.Add("Wsnet2-User", b.userId)
-	hdr.Add("Wsnet2-LastEventSeq", strconv.Itoa(seq))
-
-	authdata, err := auth.GenerateAuthData(authKey, b.userId, time.Now())
-	if err != nil {
-		log.Printf("[bot:%v] generate authdata error: %v\n", b.userId, err)
-		return nil, err
-	}
-	hdr.Add("Authorization", "Bearer "+authdata)
-
-	ws, res, err := b.ws.Dial(url, hdr)
-	if err != nil {
-		log.Printf("[bot:%v] dial error: %v, %v\n", b.userId, res, err)
-		return nil, err
-	}
-	log.Printf("[bot:%v] response: %v\n", b.userId, res)
-
-	return ws, nil
-}
-
 func main() {
 	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
 	fmt.Println("WSNet2-Bot")
 	fmt.Println("WSNet2Version:", WSNet2Version)
 	fmt.Println("WSNet2Commit:", WSNet2Commit)
 
-	bot := NewBot(appID, appKey, "12345")
+	bot := NewBot(appID, appKey, "12345", binary.Dict{})
 
-	room, err := bot.CreateRoom()
+	room, err := bot.CreateRoom(binary.Dict{"key1": binary.MarshalInt(1024), "key2": binary.MarshalStr16("hoge")})
 	if err != nil {
 		log.Printf("create room error: %v\n", err)
 		return
@@ -426,7 +179,7 @@ func main() {
 }
 
 func spawnPlayer(roomId, userId string, queries []lobby.PropQuery) {
-	bot := NewBot(appID, appKey, userId)
+	bot := NewBot(appID, appKey, userId, binary.Dict{})
 
 	room, err := bot.JoinRoom(roomId, queries)
 	if err != nil {
@@ -445,7 +198,7 @@ func spawnPlayer(roomId, userId string, queries []lobby.PropQuery) {
 }
 
 func spawnWatcher(roomId, userId string) {
-	bot := NewBot(appID, appKey, userId)
+	bot := NewBot(appID, appKey, userId, binary.Dict{})
 
 	room, err := bot.WatchRoom(roomId, nil)
 	if err != nil {
@@ -491,7 +244,7 @@ func spawnWatcher(roomId, userId string) {
 }
 
 func spawnPlayerByNumber(roomNumber int32, userId string, queries []lobby.PropQuery) {
-	bot := NewBot(appID, appKey, userId)
+	bot := NewBot(appID, appKey, userId, binary.Dict{})
 
 	room, err := bot.JoinRoomByNumber(roomNumber, queries)
 	if err != nil {
@@ -510,7 +263,7 @@ func spawnPlayerByNumber(roomNumber int32, userId string, queries []lobby.PropQu
 }
 
 func spawnPlayerAtRandom(userId string, searchGroup uint32, queries []lobby.PropQuery) {
-	bot := NewBot(appID, appKey, userId)
+	bot := NewBot(appID, appKey, userId, binary.Dict{})
 	room, err := bot.JoinRoomAtRandom(searchGroup, queries)
 	if err != nil {
 		log.Printf("[bot:%v] join room error: %v\n", userId, err)
