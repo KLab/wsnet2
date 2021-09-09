cd4be5b31e8d39150fc0da2437f85abe6b45680f
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2021-09-09 14:50:15 +0900

    static

--- wsnet2-unity/Assets/WSNet2/Scripts/Core/WSNet2Settings.cs
+++ wsnet2-unity/Assets/WSNet2/Scripts/Core/WSNet2Settings.cs
@@ -3,31 +3,31 @@ namespace WSNet2.Core
     public class WSNet2Settings
     {
         /// <summary>保持できるEventの数</summary>
-        public int EvPoolSize = 16;
+        public static int EvPoolSize = 16;
 
         /// <summary>各Eventのバッファサイズの初期値</summary>
-        public int EvBufInitialSize = 256;
+        public static int EvBufInitialSize = 256;
 
         /// <summary>保持できるMsgの数</summary>
-        public int MsgPoolSize = 16;
+        public static int MsgPoolSize = 16;
 
         /// <summary>各Msgのバッファサイズの初期値</summary>
-        public int MsgBufInitialSize = 256;
+        public static int MsgBufInitialSize = 256;
 
         /// <summary>最大連続再接続試行回数</summary>
-        public int MaxReconnection = 5;
+        public static int MaxReconnection = 5;
 
         /// <summary>接続タイムアウト</summary>
-        public int ConnectTimeoutMilliSec = 5000;
+        public static int ConnectTimeoutMilliSec = 5000;
 
         /// <summary>再接続インターバル (milli seconds)</summary>
-        public int RetryIntervalMilliSec = 1000;
+        public static int RetryIntervalMilliSec = 1000;
 
         /// <summary>最大Ping間隔 (milli seconds)</summary>
         /// Playerの最終Msg時刻のやりとりのため、ある程度で上限を設ける
-        public int MaxPingIntervalMilliSec = 10000;
+        public static int MaxPingIntervalMilliSec = 10000;
 
         /// <summary>最小Ping間隔 (milli seconds)</summary>
-        public int MinPingIntervalMilliSec = 1000;
+        public static int MinPingIntervalMilliSec = 1000;
     }
 }
