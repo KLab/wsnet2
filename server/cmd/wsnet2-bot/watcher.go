ed1da06e4523d651a517270668bc25199d053960
YAMAMOTO Masaya <yamamoto-ma@klab.com>
2021-12-08 13:13:45 +0900

    WatcherのuserIdを生成する際にプロセスIDを追加

--- server/cmd/wsnet2-bot/watcher.go
+++ server/cmd/wsnet2-bot/watcher.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"strconv"
 	"sync"
 	"time"
@@ -27,13 +28,14 @@ func (cmd *watcherBot) Execute(args []string) {
 		n, _ = strconv.Atoi(args[1])
 	}
 	logger.Infof("rid==%v, n=%v", rid, n)
+	pid := os.Getpid()
 	wg := &sync.WaitGroup{}
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func(cid int) {
 			defer wg.Done()
 			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-			watcher, err := SpawnWatcher(rid, fmt.Sprintf("watcher-%s:%03d", rid, cid))
+			watcher, err := SpawnWatcher(rid, fmt.Sprintf("watcher-%s:%d:%03d", rid, pid, cid))
 			if err != nil {
 				return
 			}
