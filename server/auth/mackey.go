adbfebce3b9e897db3d7f3e7bfcfefa545b04c77
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2021-12-13 17:32:36 +0900

    enc/dec mackey

---
+++ server/auth/mackey.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"strings"
+
+	"golang.org/x/xerrors"
+)
+
+// DecryptMACKey decodes a MACKey
+func DecryptMACKey(encMKey, key string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encMKey)
+	if err != nil {
+		return "", err
+	}
+
+	ckey := sha256.Sum256([]byte(key))
+	b, err := aes.NewCipher(ckey[:])
+	if err != nil {
+		return "", err
+	}
+
+	bs := b.BlockSize()
+	iv := data[:bs]
+	dst := make([]byte, len(data)-bs)
+
+	cipher.NewCBCDecrypter(b, iv).CryptBlocks(dst, data[bs:])
+
+	return strings.TrimRight(string(dst), "\x00"), nil
+}
+
+// EncryptMAckey encrypts macKey and returns base64 string
+func EncryptMACKey(key, macKey string) (string, error) {
+	ckey := sha256.Sum256([]byte(key))
+	b, err := aes.NewCipher(ckey[:])
+	if err != nil {
+		return "", err
+	}
+
+	bs := b.BlockSize()
+	blocks := int((len(macKey) + bs - 1) / bs)
+	src := make([]byte, blocks*bs)
+	if n := copy(src, []byte(macKey)); n != len(macKey) {
+		return "", xerrors.Errorf("copy macKey to block: %v, %v", n, len(macKey))
+	}
+
+	buf := make([]byte, bs+blocks*bs) // iv + dst
+
+	// iv
+	iv := buf[:bs]
+	if n, err := rand.Read(iv); err != nil {
+		return "", err
+	} else if n != bs {
+		return "", xerrors.Errorf("IV length %v", n)
+	}
+
+	cipher.NewCBCEncrypter(b, iv).CryptBlocks(buf[bs:], src)
+
+	return base64.StdEncoding.EncodeToString(buf), nil
+}
---
+++ server/auth/mackey_test.go
@@ -0,0 +1,22 @@
+package auth
+
+import "testing"
+
+func TestMACKey(t *testing.T) {
+	key := "testkey2"
+	mackey := "testMACKey2"
+
+	encMkey, err := EncryptMACKey(key, mackey)
+	if err != nil {
+		t.Fatalf("EncryptMACKey: %v", err)
+	}
+
+	r, err := DecryptMACKey(encMkey, key)
+	if err != nil {
+		t.Fatalf("DecryptMACKey: %v", err)
+	}
+
+	if r != mackey {
+		t.Fatalf("decrypted = %q, wants %q", r, mackey)
+	}
+}
