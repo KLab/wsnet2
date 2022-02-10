ae06dea639d98ee048279b444862612deb00b790
Inada Naoki <songofacandy@gmail.com>
2022-02-10 14:14:56 +0900

    gameのpprof用サーバーに意図的にDB接続を詰まらせるAPIを追加

--- server/game/service/pprof.go
+++ server/game/service/pprof.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
+	"time"
 
 	"wsnet2/log"
 )
@@ -14,6 +15,29 @@ func (sv *GameService) servePprof(ctx context.Context) <-chan error {
 	if sv.conf.PprofPort == 0 {
 		return nil
 	}
+
+	http.HandleFunc("/debug/stop-the-db", func(w http.ResponseWriter, r *http.Request) {
+		d := time.Second * 10
+		if p := r.URL.Query().Get("d"); p != "" {
+			var err error
+			d, err = time.ParseDuration(p)
+			if err != nil {
+				w.WriteHeader(400)
+				w.Write([]byte("bad duration"))
+				return
+			}
+		}
+		sv.db.SetMaxOpenConns(1)
+		conn, err := sv.db.Conn(context.Background())
+		if err != nil {
+			time.Sleep(d)
+			conn.Close()
+		}
+		cs := sv.conf.DbMaxConns
+		sv.db.SetMaxOpenConns(cs)
+		sv.db.SetMaxIdleConns(cs)
+		_, _ = w.Write([]byte(fmt.Sprintf("%+v", sv.db.Stats())))
+	})
 
 	errCh := make(chan error)
 
