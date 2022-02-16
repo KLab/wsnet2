052e5a9a5d5da9ffeab15eab51dad37660ee0472
Inada Naoki <songofacandy@gmail.com>
2022-02-16 11:13:09 +0900

    /debug/stop-the-db の改良

--- server/game/service/pprof.go
+++ server/game/service/pprof.go
@@ -16,6 +16,7 @@ func (sv *GameService) servePprof(ctx context.Context) <-chan error {
 		return nil
 	}
 
+	// DB接続を擬似的に詰まった状態にする
 	http.HandleFunc("/debug/stop-the-db", func(w http.ResponseWriter, r *http.Request) {
 		d := time.Second * 10
 		if p := r.URL.Query().Get("d"); p != "" {
@@ -27,16 +28,27 @@ func (sv *GameService) servePprof(ctx context.Context) <-chan error {
 				return
 			}
 		}
-		sv.db.SetMaxOpenConns(1)
+
+		// SetMaxOpenConns(0) は無制限にDB接続することになる。
+		// 1本の接続を握って SetMaxOpenConns(1) することでDB接続が詰まった状況を作る。
 		conn, err := sv.db.Conn(context.Background())
 		if err != nil {
-			time.Sleep(d)
-			conn.Close()
+			log.Errorf("/debug/stop-the-db: failed to get db conn: %v", err)
+
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("failed to get db conn: %v\n", err)))
+			return
 		}
+
+		sv.db.SetMaxOpenConns(1)
+		time.Sleep(d)
+
+		conn.Close()
 		cs := sv.conf.DbMaxConns
 		sv.db.SetMaxOpenConns(cs)
 		sv.db.SetMaxIdleConns(cs)
-		_, _ = w.Write([]byte(fmt.Sprintf("%+v", sv.db.Stats())))
+
+		_, _ = w.Write([]byte(fmt.Sprintf("%+v\n", sv.db.Stats())))
 	})
 
 	errCh := make(chan error)
