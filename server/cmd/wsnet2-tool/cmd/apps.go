38346936dca380cf8181080e44ef5f251863cd9c
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2022-02-24 20:13:11 +0900

    subcommand apps/servers

---
+++ server/cmd/wsnet2-tool/cmd/apps.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/cobra"
+)
+
+type app struct {
+	Id   string `db:"id"`
+	Name string `db:"name"`
+	Key  string `db:"key"`
+}
+
+// appsCmd represents the apps command
+var appsCmd = &cobra.Command{
+	Use:   "apps",
+	Short: "Show applications",
+	Long:  "Show applications registered on the DB",
+	Run: func(cmd *cobra.Command, args []string) {
+		const sql = "SELECT `id`, `key`, `name` FROM `app`"
+
+		var apps []*app
+		err := sqlx.SelectContext(cmd.Context(), db, &apps, sql)
+		if err != nil {
+			panic(err)
+		}
+
+		if verbose {
+			fmt.Println("id\tkey\tname")
+		}
+
+		for _, app := range apps {
+			fmt.Printf("%s\t%s\t%q\n", app.Id, app.Key, app.Name)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(appsCmd)
+}
--- server/cmd/wsnet2-tool/cmd/root.go
+++ server/cmd/wsnet2-tool/cmd/root.go
@@ -5,26 +5,46 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/spf13/cobra"
+
+	"wsnet2/config"
 )
 
-
+var (
+	confFile string
+	conf     *config.Config
+	db       *sqlx.DB
+	verbose  bool
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "wsnet2-tool",
-	Short: "A brief description of your application",
+	Short: "WSNet2 tool",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		conf, err = config.Load(confFile)
+		if err != nil {
+			return err
+		}
+		db, err = sqlx.Open("mysql", conf.Db.DSN())
+		if err != nil {
+			return err
+		}
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -32,6 +52,7 @@ to quickly create a Cobra application.`,
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "%+v", err)
 		os.Exit(1)
 	}
 }
@@ -41,11 +62,6 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.wsnet2-tool.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&confFile, "config", "f", "", "Config toml file")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Verbose output")
 }
-
-
