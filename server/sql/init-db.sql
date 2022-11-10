cf5b950a9a5b39fc89330bd6e11c1b4f4819eb65
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2022-11-10 02:58:34 +0000

    不要ファイル削除

--- server/sql/init-db.sql
+++
@@ -1,5 +0,0 @@
-create database wsnet charset utf8mb4;
-create user 'wsnet'@'localhost' identified by 'wsnetpass';
-grant all on wsnet.* to 'wsnet'@'localhost';
-grant all on wsnet.* to 'wsnet'@'%';
-
