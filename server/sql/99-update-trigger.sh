51f6c94c36bfb8668d9e84238d6e2ed124cfeb0c
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2023-02-07 03:19:54 +0000

    Linux以外でのDockerを考慮

--- server/sql/99-update-trigger.sh
+++ server/sql/99-update-trigger.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 SCHEMA=/docker-entrypoint-initdb.d/10-schema.sql
 TRIGGER=/trigger.d/db.trigger
-[ ! -e $TRIGGER -o $SCHEMA -nt $TRIGGER ] && touch $TRIGGER || true
+[[ ! -e $TRIGGER || $SCHEMA -nt $TRIGGER ]] && touch $TRIGGER || true
