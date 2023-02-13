88f042dae071142016759309868b9f13867f5fcf
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2023-02-13 05:30:27 +0000

    build system

--- wsnet2-dashboard/backend/src/plugins/binary.d.ts
+++ wsnet2-dashboard/backend/src/plugins/binary.d.ts
@@ -1,12 +1,6 @@
 declare namespace binary {
   export type Unmarshaled = [unknown, object | null];
   export function UnmarshalRecursive(src: Uint8Array): Unmarshaled;
-  export function MarshalStr8(src: string): Uint8Array;
-  export function MarshalFloat(src: number): Uint8Array;
-  export function MarshalInt(src: number): Uint8Array;
-  export function MarshalDict(src: object): Uint8Array;
-  export function MarshalList(src: Array<Uint8Array>): Uint8Array;
-  export function MarshalNull(): Uint8Array;
 }
 
 export = binary;
