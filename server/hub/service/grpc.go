3b104d1ae67f7028dd37643599d9f306e7e7cb18
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2023-04-17 11:51:12 +0000

    hub grpc logger

--- server/hub/service/grpc.go
+++ server/hub/service/grpc.go
@@ -50,17 +50,24 @@ func (sv *HubService) serveGRPC(ctx context.Context) <-chan error {
 }
 
 func (sv *HubService) Watch(ctx context.Context, in *pb.JoinRoomReq) (*pb.JoinedRoomRes, error) {
-	log.Infof("Watch request: %v, room=%v, client=%v", in.AppId, in.RoomId, in.ClientInfo.Id)
+	logger := log.GetLoggerWith(
+		log.KeyHandler, "grpc:Watch",
+		log.KeyApp, in.AppId,
+		log.KeyClient, in.ClientInfo.Id,
+		log.KeyRoom, in.RoomId,
+		log.KeyRequestedAt, float64(time.Now().UnixMilli())/1000,
+	)
+	logger.Debugf("gRPC Watch: %v %v", in.RoomId, in.ClientInfo)
 
 	res, err := sv.repo.WatchRoom(ctx, in.AppId, hub.RoomID(in.RoomId), in.ClientInfo, in.MacKey)
 	if err != nil {
-		log.Infof("watch room error: %+v", err)
+		logger.Errorf("repo.WatchRoom: %+v", err)
 		return nil, status.Errorf(err.Code(), "WatchRoom failed: %s", err)
 	}
 
 	res.Url = fmt.Sprintf(sv.wsURLFormat, res.RoomInfo.Id)
 
-	log.Infof("Watch room: room=%v, client=%v", res.RoomInfo.Id, in.ClientInfo.Id)
+	logger.Infof("gRPC Watch OK: room=%v user=%v", res.RoomInfo.Id, in.ClientInfo.Id)
 
 	return res, nil
 }
