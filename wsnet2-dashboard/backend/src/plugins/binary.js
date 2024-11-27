aa1016b3955f386d22d6c58dfd7b22427212b511
Daisuke MAKIUCHI <makiuchi-d@klab.com>
2024-11-27 18:38:14 +0900

    wasm loader

---
+++ wsnet2-dashboard/backend/src/plugins/binary.js
@@ -0,0 +1,18 @@
+import "./wasm_exec.js";
+import fs from "node:fs";
+import path from "node:path";
+
+const dir = path.dirname(new URL(import.meta.url).pathname);
+const go = new Go();
+
+const wasm = await WebAssembly.instantiate(fs.readFileSync(path.resolve(dir, "binary.wasm")), go.importObject);
+go.run(wasm.instance);
+const binary = globalThis.binary;
+
+export function UnmarshalRecursive(src) {
+    const ret = binary.UnmarshalRecursive(src);
+    if (ret.err != null) {
+        return [null, ret.err]
+    }
+    return [JSON.parse(ret.val), null];
+}
